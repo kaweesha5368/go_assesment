0756e98d396d9081583122df451f344529397082
kaweesha5368 <theekshana.a@botcalm.com>
2026-07-14 17:04:54 +0530

    Add files via upload

--- internal/blockchain/types.go
+++ internal/blockchain/types.go
@@ -5,12 +5,17 @@ type Transaction struct {
     Sender    string `json:"sender"`
     Recipient string `json:"recipient"`
     Amount    int64  `json:"amount"`
+    Nonce     int64  `json:"nonce"`      // prevent replay
+   // PubKey    []byte `json:"pubkey"`     // compressed/uncompressed public key
+   //SigR      []byte `json:"sig_r"`      // ECDSA r
+   // SigS      []byte `json:"sig_s"`      // ECDSA s
 }
 
 type Block struct {
     Index        uint64        `json:"index"`
     Timestamp    int64         `json:"timestamp"`
     Txns         []Transaction `json:"txns"`
+    MerkleRoot   string        `json:"merkle_root"`
     Hash         string        `json:"hash"`
     Nonce        uint64        `json:"nonce"`
     PreviousHash string        `json:"previous_hash"`
