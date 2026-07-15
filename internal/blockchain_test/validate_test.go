baf6fb564642837f5448c5d221c3ecf7d33315ba
kaweesha5368 <theekshana.a@botcalm.com>
2026-07-15 12:02:03 +0530

    correcting validate_test.go
    corrected the index of test transaction hash test from blk.Txns[1].Amount = 1003 --> blk.Txns[0].Amount=1003 as every test a new block is created and it only has one transaction

--- internal/blockchain_test/validate_test.go
+++ internal/blockchain_test/validate_test.go
@@ -1,7 +1,8 @@
-
 package blockchain_test
 
 import (
+	//"fmt"
+	"fmt"
 	"testing"
 	"time"
 
@@ -9,7 +10,7 @@ import (
 	"strings"
 )
 
-func TestValidateChain_TamperedHash (t *testing.T){
+func TestValidateChain_TamperedHash(t *testing.T) {
 	withDifficulty := func(d int, fn func()) {
 		old := blockchain.Difficulty
 		blockchain.Difficulty = d
@@ -23,10 +24,12 @@ func TestValidateChain_TamperedHash (t *testing.T){
 			{Sender: "coinbase", Recipient: "alice", Amount: 1002},
 		}, time.Now().Unix())
 		// tamper
-		blk.Txns[1].Amount = 1003
+		fmt.Println(blk.Txns[0].Amount)
+		blk.Txns[0].Amount = 1003
+		//when blk.Txns is 1002 test fails as it can't find a mismatch between the original and modified amounts
 		chain := blockchain.Chain{Blocks: []blockchain.Block{g, blk}}
 		ok, err := blockchain.ValidateChain(&chain)
-
+        
 		if ok {
 			t.Fatalf("expected ValidateChain to return ok=false for tampered chain")
 		}
